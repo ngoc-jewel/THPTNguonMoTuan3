e551e2249cbe3fdb4bf599d1db8c6448fad5457c
Ngoc <baongoc.91735@gmail.com>
2025-11-04 11:34:26 +0700

    A

--- Source.cpp
+++ Source.cpp
@@ -2,13 +2,18 @@
 
 using namespace std;
 int tong(int a, int b);
+{
+	rerun a+b;
+}
 int hieu(int a, int b);
 int tich(int a, int b);
 float thuong(int a, int b);
 int main()
 {
-	cout << "DAY LA CHUONG TRINH DE THUC HANH GIT";
-	cout<<"Tong =";
+	int x,y;
+	cout<<" Nhap Hai So:";
+	cin>>x,y;
+	cout<<"Tong="<<tong(x,y)<<endl;
 	system("pause");
 	return 0;
 }
