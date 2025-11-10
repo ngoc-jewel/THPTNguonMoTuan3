d90973d6bd471eef722d5283ebe360beb897b199
ngoc-jewel <71751670+ngoc-jewel@users.noreply.github.com>
2025-11-11 00:33:56 +0700

    init

--- Source.cpp
+++ Source.cpp
@@ -1,19 +1,36 @@
 #include <iostream>
-
 using namespace std;
-int tong(int a, int b);
-{
-	rerun a+b;
+
+int tong(int a, int b) {
+    return a + b;
 }
-int hieu(int a, int b);
-int tich(int a, int b);
-float thuong(int a, int b);
-int main()
-{
-	int x,y;
-	cout<<" Nhap Hai So:";
-	cin>>x,y;
-	cout<<"Tong="<<tong(x,y)<<endl;
-	system("pause");
-	return 0;
+
+int hieu(int a, int b) {
+    return a - b;
+}
+
+int tich(int a, int b) {
+    return a * b;
+}
+
+float thuong(int a, int b) {
+    if (b == 0) {
+        cout << "Khong the chia cho 0!" << endl;
+        return 0;
+    }
+    return (float)a / b;
+}
+
+int main() {
+    int x, y;
+    cout << "Nhap hai so: ";
+    cin >> x >> y;
+
+    cout << "Tong = " << tong(x, y) << endl;
+    cout << "Hieu = " << hieu(x, y) << endl;
+    cout << "Tich = " << tich(x, y) << endl;
+    cout << "Thuong = " << thuong(x, y) << endl;
+
+    system("pause");
+    return 0;
 }
